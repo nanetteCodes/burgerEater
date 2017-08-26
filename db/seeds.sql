83ee6c4135428c2cf4ae4e08c761068d4f6d958e
nanette julius <nanettejulius@nanettes-MacBook-Pro-3.local>
2017-08-25 18:35:46 -0700

    inital commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(burger_name) VALUES ('Good Burger');
+INSERT INTO burgers(burger_name) VALUES ('Krusty Krabs Burger');
+INSERT INTO burgers(burger_name) VALUES ('Bison Burger');
+INSERT INTO burgers(burger_name) VALUES ('Double Bacon Cheeseburger');
